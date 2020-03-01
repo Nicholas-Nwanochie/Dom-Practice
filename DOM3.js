6e6ee863f387f84b6e6a3688e4ea779f6f93b22b
NICHOLAS NWANOCHIE <nickclay@live.com>
2020-03-01 09:41:50 -0600

    Create DOM3.js

---
+++ DOM3.js
@@ -0,0 +1,32 @@
+var headOne = document.querySelector("#one")
+var headTwo = document.querySelector("#two")
+var headThree = document.querySelector("#three")
+
+headOne.addEventListener("mouseover",function(){
+headOne.textContent = " mouse currently over";
+headOne.style.color = "red";
+
+})
+
+headOne.addEventListener("mouseout",function(){
+  headOne.textContent = "HOVER OVER ME!";
+  headOne.style.color = "black"
+})
+
+
+
+
+
+
+
+headTwo.addEventListener("click",function(){
+  headTwo.textContent = "CLICKED ON ";
+  headTwo.style.color = "blue";
+})
+
+
+headThree.addEventListener("dblclick",function(){
+
+  headThree.textContent = "i was doube clicked!";
+  headThree.style.color = "red";
+})
